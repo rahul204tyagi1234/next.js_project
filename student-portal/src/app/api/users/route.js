6c29e25f7a63dabada924fb930c70daba3c061b7
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2024-01-11 20:47:03 +0530

    connect to mongodb

---
+++ student-portal/src/app/api/users/route.js
@@ -0,0 +1,26 @@
+import { User } from "@/app/lib/model/connect";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    let data=[];
+    let success=true;
+    try {
+        await mongoose.connect("mongodb://localhost:27017");
+        data=await User.find();
+        console.log(data);
+    } catch (error) {
+        data={result:"error"}
+        success=false
+    }
+
+    return NextResponse.json({result:data ,success})
+}
+
+export async function POST(request){
+    const payload= await request.json();
+    await mongoose.connect("mongodb://localhost:27017");
+    let users=new User(payload);
+    let result= await users.save();
+    return NextResponse.json({result,success:true})
+}
